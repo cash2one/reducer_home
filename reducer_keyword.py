375ac6803efedc50a7fe645770d15ce21a04377f
wentao.ma <wentao.ma@dianping.com>
2013-06-25 15:59:56 +0800

    cityid

--- reducer_keyword.py
+++ reducer_keyword.py
@@ -1,43 +1,39 @@
-# -*- coding: utf-8 -*-
 #!/bin/env python
+# -*- coding: utf-8 -*-
 import urllib
-import re
 import sys
+import re
 from sys import stdin
-
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 pat1 = u'搜索商户'
 patE = r'^[0-9a-zA-Z \']+$'
-shopid = 0
-f = stdin
+shopid=0
+f=stdin
 for line in f:
-    # print  line
-    try:
-        guid = line.strip().split('\t')[0]
-        city = line.strip().split('\t')[1]
-        url = line.strip().split('\t')[2]
-        keyword = urllib.unquote(url)
-        if type(keyword) == "str":
-            keyword = keyword.decode("utf-8", "ignore")
-        keyword = keyword.replace(u'、',u' ').replace(u'，', u' ').replace(u'.', u' ').replace(u',', u' ').replace(u'。', u' ').replace(u'#', u' ')
-        # for key in keyword.split():
-        k = len(keyword.split())
-        if re.match(patE,keyword):
-            print "\t".join([guid, city, keyword])
-        elif ((k >= 1) and (k <= 3)) and not re.search(pat1,keyword):
-            for key in keyword.split():
-                if len(key) >=2 and len(key) <=7  :
-                    print "\t".join([guid, city, key])
-
-    except IndexError, ex:
-        pass
-    except ValueError, ex:
-        pass
-    finally:
-        tagdict = {}
-        shopid = 0
+  try:
+    guid = line.strip().split('\t')[0]
+    city= line.strip().split('\t')[1]
+    url = line.strip().split('\t')[2]
+    keyword = urllib.unquote(url)
+    if type(keyword)=="str":
+      keyword = keyword.decode("utf-8","ignore")
+    keyword = keyword.replace(u'、',u' ').replace(u'，', u' ').replace(u'.', u' ').replace(u',', u' ').replace(u'。', u' ').replace(u'#', u' ')
+    k = len(keyword.split())
+    if re.match(patE, keyword):
+      print "\t".join([guid, city, keyword])
+    elif ((k >= 1) and (k <= 3)) and not re.search(pat1,keyword):
+      for key in keyword.split():
+        if len(key) >=2 and len(key) <=7:
+          print "\t".join([guid, city, key])
+  except IndexError,ex:
+    pass
+  except ValueError,ex:
+    pass
+  finally:
+    tagdict={}
+    shopid=0
 
 def is_chinese(uchar):
     if uchar >= u'\u4e00' and uchar<=u'\u9fa5':
