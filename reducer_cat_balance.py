3d555968fc45bc406dd8492270193fb3dec77ee1
wentao.ma <wentao.ma@dianping.com>
2013-06-25 17:19:57 +0800

    add reducer_cat_balance.py

---
+++ reducer_cat_balance.py
@@ -0,0 +1,41 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from sys import stdin
+reload(sys)
+sys.setdefaultencoding('utf-8')
+from string import join
+
+tagdict={}
+
+shopid=0
+cityid=0
+
+def output(tagdict):
+  try:
+    if cityid==0: return
+    if len(tagdict)==0: return
+    if shopid==0: return
+    for key in tagdict:
+        print "\t".join([cityid,shopid,key.encode('utf-8'),tagdict[key]])
+  finally:
+    pass
+
+currentid = None
+cat = {}
+
+for line in stdin:
+  try:
+    line=line.decode('utf-8')
+    deviceid,shopid,rank,cat1 = line.strip().split()
+    if deviceid != deviceid_old:
+      deviceid_old = deviceid
+      print 'id changed'
+
+  except IndexError,ex:
+    pass
+  except ValueError,ex:
+    pass
+  finally:
+    tagdict={}
+    shopid=0
