d0c162a735d674c3194df81bbb14d0d41dc5cbc2
wentao.ma <wentao.ma@dianping.com>
2013-06-26 21:59:02 +0800

    fix reducer_keyword.py

--- reducer_keyword.py
+++ reducer_keyword.py
@@ -6,9 +6,8 @@ import re
 from sys import stdin
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
 pat1 = u'搜索商户'
-patE = r'^[0-9a-zA-Z \']+$'
+patE = r'^[0-9a-zA-Z \'\.]+$'
 shopid=0
 f=stdin
 for line in f:
@@ -19,11 +18,13 @@ for line in f:
     keyword = urllib.unquote(url)
     if type(keyword)=="str":
       keyword = keyword.decode("utf-8","ignore")
-    keyword = keyword.replace(u'、',u' ').replace(u'，', u' ').replace(u'.', u' ').replace(u',', u' ').replace(u'。', u' ').replace(u'#', u' ')
     k = len(keyword.split())
-    if re.match(patE, keyword):
+    if re.match(patE, keyword) and len(keyword) >=5 :
+      # print len(keyword)
       print "\t".join([guid, city, keyword])
-    elif ((k >= 1) and (k <= 3)) and not re.search(pat1,keyword):
+    elif ((k >= 1) and (k <= 2)) and not re.search(pat1,keyword) and not re.match(patE, keyword) :
+      # print keyword
+      keyword = keyword.replace(u'、',u' ').replace(u'，', u' ').replace(u'.', u' ').replace(u',', u' ').replace(u'。', u' ').replace(u'#', u' ')
       for key in keyword.split():
         if len(key) >=2 and len(key) <=7:
           print "\t".join([guid, city, key])
@@ -34,19 +35,3 @@ for line in f:
   finally:
     tagdict={}
     shopid=0
-
-def is_chinese(uchar):
-    if uchar >= u'\u4e00' and uchar<=u'\u9fa5':
-        return True
-    else:
-        return False
-
-
-def is_english(uchar):
-    if (uchar >= u'\u0041' and uchar<=u'\u005a') or (uchar >= u'\u0061' and uchar<=u'\u007a'):
-        return True
-    else:
-        return False
-
-#中文数字 去标点符号，分开，长度限制
-#英文 什么都不干
