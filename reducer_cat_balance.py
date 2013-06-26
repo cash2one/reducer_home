a6b821396c2f5c295d318b7d6a02653cd9c8d3d5
wentao.ma <wentao.ma@dianping.com>
2013-06-26 14:25:38 +0800

    add reducer_cat_balance.py

--- reducer_cat_balance.py
+++ reducer_cat_balance.py
@@ -11,26 +11,41 @@ tagdict={}
 shopid=0
 cityid=0
 
-def output(tagdict):
+def output(catlist , catshop):
   try:
-    if cityid==0: return
-    if len(tagdict)==0: return
-    if shopid==0: return
-    for key in tagdict:
-        print "\t".join([cityid,shopid,key.encode('utf-8'),tagdict[key]])
+    # print catlist
+    # print catshop
+    while len(catlist)>0:
+        for cat1 in catlist:
+            if len(catshop[cat1])>0:
+                (shopid,rank) = catshop[cat1].pop(0)
+                print "\t".join([deviceid_old,shopid,cat1,rank])
+            else:
+                catlist.remove(cat1)
   finally:
     pass
 
 currentid = None
-cat = {}
 deviceid_old = None
+
+catshop = {}
+catlist = []
 for line in stdin:
   try:
     line=line.decode('utf-8')
-    deviceid,shopid,rank = line.strip().split()
+    deviceid,shopid,cat1,rank = line.strip().split()
     if deviceid != deviceid_old:
       deviceid_old = deviceid
       print 'id changed',deviceid
+      # print catshop
+      if len(catlist) > 0:
+          output(catlist,catshop)
+      catshop = {}
+      catlist = []
+    if cat1 not in catlist:
+        catlist.append(cat1)
+        catshop[cat1] = [tuple((shopid, rank))]
+    catshop[cat1].append(tuple((shopid, rank)))
 
   except IndexError,ex:
     pass
