7672a8b4fab10b99986655dcd5b7f93c12c2ddb0
wentao.ma <wentao.ma@dianping.com>
2013-07-19 11:09:04 +0800

    no message

--- sync/div.py
+++ sync/div.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 #!/bin/env python
 
-outfile = file("mobile_update_part.txt","w")
+outfile = file("mobile_base_part.txt","w")
 count = 0
 shopbefore = 0
 tag = False
-for line in file("test.txt"):
+for line in file("/Users/mantou/mobile_base.txt"):
     count += 1
     shopid = line.split('\t')[0]
-    if count > 20:
+    if count > 1210000:
         if shopid != shopbefore:
             tag = True
         if tag:
